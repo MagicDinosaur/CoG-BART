f1f9672003a9b5e66b65c471b8376f6859d96f17
DucPham2112 <94766118+MagicDinosaur@users.noreply.github.com>
2022-03-20 21:48:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -147,8 +147,8 @@ if __name__ == '__main__':
 
     model_args, data_args, training_args, other_args = parser.parse_args_into_dataclasses()
     # Set seed before initializing model.
-
-    fitlog.set_log_dir('/remote-home/smli/Project/CoG-BART/logs/')
+    # Should change the directory to avoid "non exist folder" at the quick start
+    fitlog.set_log_dir('/CoG-BART/logs/')
     rnd_seed = fitlog.set_rng_seed() if training_args.seed == -1 else fitlog.set_rng_seed(training_args.seed)
     # training_args.seed = rnd_seed
 
